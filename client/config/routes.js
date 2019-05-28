070c121eb9f03eb76c0b8d8ba9dc113f895fe70a
wangxuexuan <wangxuexuan@yahoo.com>
2019-05-28 21:56:33 +0800

    自定义组件

--- client/config/routes.js
+++ client/config/routes.js
@@ -1,5 +1,5 @@
-// import Todo from './../views/todo/todo.vue'
-// import Login from './../views/login/login.vue'
+import Todo from './../views/todo/todo.vue'
+import Login from './../views/login/login.vue'
 
 export default [
   {
@@ -11,7 +11,8 @@ export default [
     // props: (route) => ({ id: route.query.name }),
     props: true,
     // path: '/app',
-    component: import('../views/todo/todo.vue'),
+    // component: import('../views/todo/todo.vue'),
+    component: Todo,
     name: 'app',
     meta: {
       title: 'this is an app',
@@ -24,6 +25,6 @@ export default [
   },
   {
     path: '/login',
-    component: './../views/login/login.vue'
+    component: Login
   }
 ]
---
+++ client/views/login/test.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+      <span>{{ label }}</span>
+      <input
+        v-bind="$attrs"
+      >
+    </div>
+</template>
+
+<script>
+export default {
+  props:['label'],
+  inheritAttrs: false, //此处设置禁用继承特性
+  mounted: function() {
+    console.log(this.$attrs);
+  }
+}
+</script>
